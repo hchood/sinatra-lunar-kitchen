fb89fd5aa9758596f702b8821fd617bc3f68cf7b
Helen Hood <helen.c.hood@gmail.com>
2014-09-09 16:26:21 -0400

    Passing tests

--- models/ingredient.rb
+++ models/ingredient.rb
@@ -1,2 +1,15 @@
-class Ingredient
+class Ingredient < ActiveRecord::Base
+  belongs_to :recipe
+
+  # the belongs_to line adds a method something like this,
+  #  if we were using the PG gem:
+  # def recipe
+  #   query = "SELECT * FROM recipes WHERE id = $1"
+  #
+  #   results = db_connection do |conn|
+  #     conn.exec(query, [self.recipe_id])
+  #   end
+  #
+  #   results.to_a.first
+  # end
 end
--- models/recipe.rb
+++ models/recipe.rb
@@ -1,30 +1,9 @@
-require 'sinatra/activerecord'
-
 class Recipe < ActiveRecord::Base
-  # def self.db_connection
-  #   begin
-  #     connection = PG.connect(dbname: 'recipes')
-  #     yield(connection)
-  #   ensure
-  #     connection.close
-  #   end
-  # end
-  #
-  # def self.all
-  #   results = db_connection do |conn|
-  #     conn.exec("SELECT * FROM recipes;")
-  #   end
-  #
-  #   results.to_a
-  # end
-  #
-  # def self.find(id)
-  #   query = "SELECT * FROM recipes WHERE id = $1"
-  #
-  #   results = db_connection do |conn|
-  #     conn.exec(query, [id])
-  #   end
-  #
-  #   results.to_a.first
+  has_many :ingredients
+
+  # the has_many line adds an instance method called ingredients:
+  # def ingredients
+  #   # runs a SQL query to get all of the ingredients for this recipe
+  #   # from the ingredients table, returning an array of Ingredient objects
   # end
 end
